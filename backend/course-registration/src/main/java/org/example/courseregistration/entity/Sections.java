2a2055567dd707921ea4444b0465cdac70defc47
yeongsinkeem <dudtls0522@sookmyung.ac.kr>
2025-06-12 16:21:44 +0900

    feat: 엔티티 생성 및 oracle 연결 설정

---
+++ backend/course-registration/src/main/java/org/example/courseregistration/entity/Sections.java
@@ -0,0 +1,46 @@
+package org.example.courseregistration.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "Sections")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Sections {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "c_id_no", nullable = false)
+    private Courses course;
+
+    @Column(name = "c_id", length = 50, nullable = false)
+    private String courseName;
+
+    @Column(name = "se_year", nullable = false)
+    private Integer year;
+
+    @Column(name = "se_semester", nullable = false)
+    private Integer semester;
+
+    @Column(name = "se_section", nullable = false)
+    private Integer section;
+
+    @Column(name = "se_time", length = 20, nullable = false)
+    private String time;
+
+    @Column(name = "se_capacity", nullable = false)
+    private Integer capacity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "p_id", nullable = false)
+    private Professors professor;
+
+    @Column(name = "se_classroom", length = 30)
+    private String classroom;
+}
+
