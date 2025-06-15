8b6961e266f98f7696bdf1a384c17f143bd9d620
Hanmh111 <96728777+Hanmh111@users.noreply.github.com>
2025-06-15 15:36:01 +0900

    Fix: 뷰 내용 수정

--- backend/course-registration/db/time_table_view.sql
+++ backend/course-registration/db/time_table_view.sql
@@ -1,12 +1,22 @@
-CREATE OR REPLACE VIEW V_StudentTimetable AS
+CREATE
+OR REPLACE VIEW V_StudentTimetable AS
 SELECT
-    e.s_id AS student_id,
-    se.se_time AS time,
-    c.C_ID_NO AS course_id,
-    c.C_ID AS course_name,
-    se.se_section AS section,
-    c.c_unit AS credit,
-    se.se_classroom AS classroom
-FROM enroll e
-         JOIN sections se ON e.e_section = se.id
-         JOIN courses c ON se.c_id_no = c.c_id_no;
+  e.s_id AS student_id,
+  e.e_year AS year,
+  e.e_semester AS semester,
+  c.c_id_no AS courseIdNo,
+  c.c_id AS courseName,
+  e.e_section AS section,
+  c.c_unit AS credit,
+  c.c_major AS major,
+  p.p_name AS professorName,
+  se.se_time AS time,
+  se.se_classroom AS classroom
+FROM
+  Enroll e
+  JOIN Courses c ON e.c_id_no = c.c_id_no
+  JOIN Sections se ON se.c_id_no = e.c_id_no
+  AND se.se_section = e.e_section
+  AND se.se_year = e.e_year
+  AND se.se_semester = e.e_semester
+  JOIN Professors p ON se.p_id = p.p_id;
